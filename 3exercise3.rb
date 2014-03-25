3817913c4a67a27d5b77ff147f6fee8303f53bab
stasdimos <stas.dimos@icloud.com>
2014-03-25 14:13:42 -0400

    4'

--- 3exercise3.rb
+++ 3exercise3.rb
@@ -1,5 +1,8 @@
 puts "What is your name?"
 name = gets.chomp
-
 puts "Hi #{name}!"
+
+puts "How old are you?"
+age = gets.to_i
+puts "You were born in #{2014 - age}!"
 
---
+++ 4.rb
@@ -0,0 +1,12 @@
+x = 3
+y = -4
+def check_sign(number)
+  if number > 0
+    "#{number} is positive"
+  else
+    "#{number} is negative"
+  end
+end
+
+check_sign(x) # 3 is positive
+check_sign(y) # -4 is negative
