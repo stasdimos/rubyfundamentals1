5032014f722fb6708383dace2fd01bfd6cc5014e
stasdimos <stas.dimos@icloud.com>
2014-03-25 13:42:49 -0400

    update 2

--- exercise2.rb
+++ exercise2.rb
@@ -1,2 +1,8 @@
-puts "a good tip is 20% or #{55 * 0.2} on a $55 bill."
+# is 20% a good tip? Too much? Too late?
+puts "a good tip is 20% or $#{55 * 0.2} on a $55 bill."
 
+# adding them intergers and strings yo
+puts = 5 + "5"
+
+# Question 3!
+puts 45628 * 7839
