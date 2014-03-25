c1a30d7ed44d8368d41ff359bdb02c8d6f97dff8
stasdimos <stas.dimos@icloud.com>
2014-03-25 15:04:13 -0400

    4

--- 4.rb
+++ 4.rb
@@ -1,12 +1,12 @@
-x = 3
-y = -4
-def check_sign(number)
-  if number > 0
-    "#{number} is positive"
-  else
-    "#{number} is negative"
-  end
-end
+one_to_onehundred = (1..100)
 
-check_sign(x) # 3 is positive
-check_sign(y) # -4 is negative
+one_to_onehundred.each do |i|
+
+	if i % 3 == 0 && i % 5 == 0
+			puts "Bitmaker"
+		elsif i % 3 == 0
+			puts "Bit"
+		else i % 5 == 0
+			puts "Maker"
+	end
+end
