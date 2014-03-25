014a478b37ccaee51914cffa4ad76dd2d4c89dfb
stasdimos <stas.dimos@icloud.com>
2014-03-25 14:04:51 -0400

    update 3

--- exercise2.rb
+++ exercise2.rb
@@ -2,7 +2,10 @@
 puts "a good tip is 20% or $#{55 * 0.2} on a $55 bill."
 
 # adding them intergers and strings yo
-puts = 5 + "5"
+puts 5.to_s + "5"
 
 # Question 3!
 puts 45628 * 7839
+
+# Value of Expression
+puts (true && false) || (false && true) || !(false && false)
